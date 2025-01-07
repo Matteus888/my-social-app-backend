bd93a665fb9f0a0dfbeb192d8f2ea487377dd088
Matthieu Chatelin <matthieu888@gmail.com>
2025-01-07 18:51:24 +0100

    Fix regex on users/signup & signin route

--- routes/users.js
+++ routes/users.js
@@ -6,20 +6,14 @@ const User = require("../models/users");
 const uid2 = require("uid2");
 const bcrypt = require("bcrypt");
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
-
 // Route pour s'inscrire sur le site
 router.post("/signup", async (req, res) => {
   if (!checkBody(req.body, ["username", "email", "password", "firstname", "lastname"])) {
     res.json({ result: false, error: "❌ BACKEND users/signup: Please complete all fields." });
     return;
   }
-
   try {
-    const user = await User.findOne({ email: { $regex: new RegExp(req.body.email, "i") } });
+    const user = await User.findOne({ email: { $regex: new RegExp(`^${req.body.email}$`, "i") } });
 
     if (user === null) {
       const newUser = new User({
@@ -35,7 +29,6 @@ router.post("/signup", async (req, res) => {
           location: req.body.location,
         },
       });
-
       try {
         const newUserDoc = await newUser.save();
         res.json({ result: true, user: newUserDoc }); // A adapter pour les besoins du frontend
@@ -57,9 +50,8 @@ router.post("/signin", async (req, res) => {
     res.json({ result: false, error: "❌ BACKEND users/signup: Please complete all fields." });
     return;
   }
-
   try {
-    const user = await User.findOne({ email: { $regex: new RegExp(req.body.email, "i") } });
+    const user = await User.findOne({ email: { $regex: new RegExp(`^${req.body.email}$`, "i") } });
 
     if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
       res.json({ result: true, user: user }); // A dapter pour les besoins du frontend
