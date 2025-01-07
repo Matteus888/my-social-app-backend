d81c823072b046c4d546955649d64bdf23d2dcaa
Matthieu Chatelin <matthieu888@gmail.com>
2025-01-07 15:16:36 +0100

    Connection to MongoDB setup

---
+++ models/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const connectionString = process.env.DATABASE_CONNECTION_STRING;
+
+mongoose
+  .connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log("✅ my-social-app database connected"))
+  .catch((error) => console.error("❌ Error to connect database", error));
