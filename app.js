76c00534300479e1a04cf7fdf025d2fd9e9b5a2b
Matthieu Chatelin <matthieu888@gmail.com>
2025-01-17 11:50:50 +0100

    Create new /auth family route for signup & signin

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 require("dotenv").config();
 require("./models/connection");
 
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var postsRouter = require("./routes/posts");
+const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
+const usersRouter = require("./routes/users");
+const postsRouter = require("./routes/posts");
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/auth", authRouter);
 app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
 
