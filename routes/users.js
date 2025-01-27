6b91b10c3728a2bf943a374256f27948d7a62a15
Matthieu Chatelin <matthieu888@gmail.com>
2025-01-27 21:38:50 +0100

    Add PUT route users/profile/image

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,65 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 const { authenticate } = require("../modules/authenticate");
 const User = require("../models/users");
+
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: (req, file) => ({
+    folder: `my-social-app/user_profile/${req.user.publicId}`, // Répertoire dynamique basé sur publicId
+    allowed_formats: ["jpg", "png", "jpeg"],
+  }),
+});
+
+const upload = multer({ storage });
+
+// Route pour mettre à jour les images de profils
+router.put("/profile/image", authenticate, upload.single("image"), async (req, res) => {
+  const { field } = req.body;
+  const publicId = req.user.publicId;
+
+  if (!["avatar", "backgroundImage"].includes(field)) {
+    return res.status(400).json({ result: false, error: "Invalid input" });
+  }
+
+  try {
+    const user = await User.findOne({ publicId: publicId });
+
+    if (!user) {
+      return res.status(404).json({ result: false, error: "User not found." });
+    }
+
+    const oldImageUrl = user.profile[field];
+
+    if (oldImageUrl) {
+      const regex = /\/upload\/(?:v\d+\/)?(.+)\.\w+$/;
+      const match = oldImageUrl.match(regex);
+
+      if (match && match[1]) {
+        const oldImagePublicId = match[1];
+        await cloudinary.uploader.destroy(oldImagePublicId);
+      }
+    }
+
+    const newImageUrl = req.file.path;
+
+    const updatedUser = await User.findOneAndUpdate({ publicId: publicId }, { [`profile.${field}`]: newImageUrl }, { new: true });
+
+    if (!updatedUser) {
+      return res.status(404).json({ result: false, error: "User not found." });
+    }
+
+    res.status(200).json({ result: true, message: "Picture successfully updated.", imageUrl: newImageUrl });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ result: false, error: "Server error during updating image." });
+  }
+});
 
 // Route pour rechercher parmis les utilisateurs
 router.get("/search", authenticate, async (req, res) => {
@@ -96,23 +153,21 @@ router.get("/friend-requests", authenticate, async (req, res) => {
   }
 });
 
-// Route pour mettre à jour le profils utilisateur A FINIR AVEC IMAGE
+// Route pour mettre à jour les infos texte du profils utilisateur
 router.put("/profile", authenticate, async (req, res) => {
   const { publicId } = req.user;
-  const { bio, job, location, avatar, website, backgroundImage } = req.body;
+  const { bio, job, location, website } = req.body;
 
   try {
     const user = await User.findOne({ publicId });
     if (!user) {
       return res.status(404).json({ result: false, error: "User not found." });
     }
-    console.log(website);
+
     if (bio !== undefined) user.profile.bio = bio;
     if (job !== undefined) user.profile.job = job;
     if (location !== undefined) user.profile.location = location;
     if (website !== undefined) user.profile.website = website;
-    if (avatar !== undefined) user.profile.avatar = avatar;
-    if (backgroundImage !== undefined) user.profile.backgroundImage = backgroundImage;
 
     await user.save();
 
