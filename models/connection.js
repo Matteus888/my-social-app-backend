cbae71b587464280137524d993a113a7ce8a203f
Matthieu Chatelin <matthieu888@gmail.com>
2025-01-30 16:45:27 +0100

    Change connection options to MongoDB

--- models/connection.js
+++ models/connection.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const connectionString = process.env.DATABASE_CONNECTION_STRING;
 
 mongoose
-  .connect(connectionString, { connectTimeoutMS: 2000 })
+  .connect(connectionString, {
+    useNewUrlParser: true, // ✅ Évite les erreurs de parsing
+    useUnifiedTopology: true, // ✅ Utilise le nouveau moteur de monitoring
+    serverSelectionTimeoutMS: 5000, // ✅ Attente max avant échec
+  })
   .then(() => console.log("✅ my-social-app database connected"))
   .catch((error) => console.error("❌ Error to connect database", error));
