eb9b6b5d7c4b91cee75582a49b7c865348407fb4
Matthieu Chatelin <matthieu888@gmail.com>
2025-01-30 17:01:51 +0100

    Change connection.js

--- models/connection.js
+++ models/connection.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 const connectionString = process.env.DATABASE_CONNECTION_STRING;
 
 mongoose
-  .connect(connectionString, {
-    useNewUrlParser: true, // ✅ Évite les erreurs de parsing
-    useUnifiedTopology: true, // ✅ Utilise le nouveau moteur de monitoring
-    serverSelectionTimeoutMS: 5000, // ✅ Attente max avant échec
+  .connect(connectionString, { connectTimeoutMS: 2000 })
+  .set("debug", true)
+  .then(() => {
+    console.log("✅ my-social-app database connected");
   })
-  .then(() => console.log("✅ my-social-app database connected"))
-  .catch((error) => console.error("❌ Error to connect database", error));
+  .catch((error) => {
+    console.error("❌ Error to connect database", error);
+  });
