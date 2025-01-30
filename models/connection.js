5a9493fe13b609d7b97fe3cfec4091d3128086b8
Matthieu Chatelin <matthieu888@gmail.com>
2025-01-30 17:04:28 +0100

    Change connection.js

--- models/connection.js
+++ models/connection.js
@@ -4,10 +4,11 @@ const connectionString = process.env.DATABASE_CONNECTION_STRING;
 
 mongoose
   .connect(connectionString, { connectTimeoutMS: 2000 })
-  .set("debug", true)
   .then(() => {
     console.log("✅ my-social-app database connected");
   })
   .catch((error) => {
     console.error("❌ Error to connect database", error);
   });
+
+mongoose.set("debug", true);
