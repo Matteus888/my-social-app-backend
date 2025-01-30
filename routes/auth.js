f46d4aed6fc0f922ec39eb35e4580c0b10932152
Matthieu Chatelin <matthieu888@gmail.com>
2025-01-30 15:47:52 +0100

    Change sameSite to Strict

--- routes/auth.js
+++ routes/auth.js
@@ -51,7 +51,7 @@ router.post("/signup", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true, // Protège contre les attaques XSS
       secure: true,
-      sameSite: "none", // Empêche les attaques CSRF
+      sameSite: "Strict", // Empêche les attaques CSRF
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours
     });
 
@@ -106,7 +106,7 @@ router.post("/signin", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true, // Protège contre les attaques XSS
       secure: true,
-      sameSite: "none", // Empêche les attaques CSRF
+      sameSite: "Strict", // Empêche les attaques CSRF
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours
     });
 
