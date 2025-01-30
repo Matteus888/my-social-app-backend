5cb001ee16d02aff29cd62adbd09c05d640a7453
Matthieu Chatelin <matthieu888@gmail.com>
2025-01-30 15:35:42 +0100

    Add setHeader to app.js

--- app.js
+++ app.js
@@ -24,7 +24,10 @@ app.use(
   })
 );
 app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://my-social-app-frontend.vercel.app");
   res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   next();
 });
 app.use(logger("dev"));
--- routes/auth.js
+++ routes/auth.js
@@ -51,7 +51,7 @@ router.post("/signup", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true, // Protège contre les attaques XSS
       secure: true,
-      sameSite: "Strict", // Empêche les attaques CSRF
+      sameSite: "none", // Empêche les attaques CSRF
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours
     });
 
@@ -105,8 +105,8 @@ router.post("/signin", async (req, res) => {
     // ✅ Stocke le token dans un cookie sécurisé
     res.cookie("token", token, {
       httpOnly: true, // Protège contre les attaques XSS
-      secure: process.env.NODE_ENV === "production", // Active secure en production
-      sameSite: "Strict", // Empêche les attaques CSRF
+      secure: true,
+      sameSite: "none", // Empêche les attaques CSRF
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours
     });
 
