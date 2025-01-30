57197e37f2c5dec19791ba27a518b1e51cd00f8f
Matthieu Chatelin <matthieu888@gmail.com>
2025-01-30 16:21:36 +0100

    Add console.log to authenticate to fix cookie issue

--- modules/authenticate.js
+++ modules/authenticate.js
@@ -5,7 +5,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 // Middleware pour vérifier le token JWT dans l'entête Authorization
 function authenticate(req, res, next) {
   const token = req.cookies.token;
-
+  console.log("Cookies reçus:", req.cookies);
   if (!token) {
     console.error("No token provided.");
     return res.status(401).json({ error: "No token provided, access denied." });
