f2d2e64668fad4b901bee56f4817c0341f99f514
Matthieu Chatelin <matthieu888@gmail.com>
2025-01-31 08:54:40 +0100

    Add cors origin

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const cors = require("cors");
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://my-social-app-frontend.vercel.app",
+    origin: "https://my-social-app-frontend.vercel.app" || "http://localhost:3001",
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
