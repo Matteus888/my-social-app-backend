72fbb11e91db5c0e9524be10ee79b642c9bd0956
Matthieu Chatelin <matthieu888@gmail.com>
2025-01-31 09:01:02 +0100

    Switch to localhost cors origin

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://my-social-app-frontend.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
